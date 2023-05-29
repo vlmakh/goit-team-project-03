1c3800b1b9aecb96752cb06b4fc650f4b8473ad6
Vlad Makhnenko <vlmakh@gmail.com>
2023-05-29 13:32:00 +0300

    add  process.env..

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,8 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-axios.defaults.baseURL = 'https://pets-back-end.onrender.com';
-// axios.defaults.baseURL = process.env.REACT_APP_MAIN_URL;
+axios.defaults.baseURL = process.env.REACT_APP_MAIN_URL;
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
