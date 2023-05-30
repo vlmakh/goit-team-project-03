17d3e8956af28281efca28e90bd30a272d759b73
Vlad Makhnenko <vlmakh@gmail.com>
2023-05-30 21:01:03 +0300

    update UserData

--- src/redux/news/operations.js
+++ src/redux/news/operations.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import toast from 'react-hot-toast';
 
 axios.defaults.baseURL = process.env.REACT_APP_MAIN_URL;
+const errorMsg = "Something's wrong. Please update page and try again";
 
 export const getOurNews = createAsyncThunk(
   'api/news',
-  async (news, { rejectWithValue }) => {
+  async (news, thunkAPI) => {
     try {
       const { data } = await axios.get('/api/news/our-news');
       return data;
     } catch (error) {
-      rejectWithValue(error.message);
+      toast.error(errorMsg);
+      return thunkAPI.rejectWithValue('');
     }
   }
 );
