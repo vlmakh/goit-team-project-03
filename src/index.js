ecacadad2c5346b1ef6ababea5530791be662fb5
Vlad Makhnenko <vlmakh@gmail.com>
2023-05-30 19:23:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,13 @@ import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="command-repo-yourpet-frontend">
+    <BrowserRouter basename="/goit-team-project-03">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-      <App />
-      </PersistGate>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
