f37215907bdab3cf324cbc279ac04471f38caed3
Vlad Makhnenko <vlmakh@gmail.com>
2023-06-03 11:18:03 +0300

    update operations

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,7 +2,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-axios.defaults.baseURL = 'https://pets-back-end.onrender.com';
+// axios.defaults.baseURL = REACT_APP_MAIN_URL;
+axios.defaults.baseURL = 'https://yourpet.onrender.com';
 const errorMsg = "Something's wrong. Please update page and try again";
 
 const setAuthHeader = token => {
